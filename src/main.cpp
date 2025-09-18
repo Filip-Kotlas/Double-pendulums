99bccc45f406abdd9bfe2badf7f75302d49e4b29
Filip Kotlas <filip.kotlas.fk@gmail.com>
2025-09-18 09:44:01 +0200

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,8 @@
-#include "Window.hpp"
 #include <iostream>
+#include <filesystem>
 
 int main() {
-    try {
-        Window w(800, 600, "Menu + Image");
-        w.run();
-    } catch (const std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
-        return -1;
-    }
+    std::filesystem::path dir = "results";
+    std::cout << "Hello, world!" << std::endl;
     return 0;
 }
